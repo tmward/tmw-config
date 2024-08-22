13142eb9a6e21bf5d377ee419a704718e0d0aa43
Thomas M Ward <thomas@thomasward.com>
2024-08-22 17:23:15 -0400

    Setup plugins with init.lua and lazy.nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,7 +1,70 @@
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+    if vim.v.shell_error ~= 0 then
+        vim.api.nvim_echo({
+            { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+            { out, "WarningMsg" },
+            { "\nPress any key to exit..." },
+          },
+          true,
+          {}
+        )
+        vim.fn.getchar()
+        os.exit(1)
+    end
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- lazy.nvim wants the following so mappings are correct
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ";"
+
+-- Setup lazy.nvim
+require("lazy").setup({
+    spec = {
+        { "ellisonleao/gruvbox.nvim",
+            priority = 1000,
+            config = true,
+            opts = ... 
+        },
+        { "Olical/conjure" },
+        { "R-nvim/R.nvim",
+            lazy = false
+        },
+        -- TODO Add conform.nvim to replace neomake/neoformat
+        { "nvim-treesitter/nvim-treesitter",
+            build = ":TSUpdate",
+            config = function()
+                local configs = require("nvim-treesitter.configs")
+                configs.setup({
+                    ensure_installed = { 
+                        "bibtex", "c", "clojure", "csv", "diff", "fennel",
+                        "git_config", "gitcommit", "gitignore", "go", "gpg",
+                        "html", "janet_simple", "lua", "make",
+                        "markdown", "muttrc", "perl", "python", "r", "sql",
+                        "ssh_config", "tmux", "tsv", "vim", "vimdoc", "xml"
+                    },
+                    sync_install = false,
+                    highlight = { enable = true },
+                    indent = { enable = true },
+                })
+            end
+        },
+    },
+    -- Configure any other settings here. See the documentation for more details.
+    -- colorscheme that will be used when installing plugins.
+    install = { colorscheme = { "habamax" } },
+    -- automatically check for plugin updates
+    checker = { enabled = true },
+})
+--
+
 -- General preferences
 vim.o.number  = true -- number lines
 vim.o.mouse='n' -- resize panes with mouse in normal mode
-vim.g.mapleader = ' ' -- Space is <Leader> key
 vim.o.incsearch = true -- Highlight while searching
 vim.o.ignorecase = true -- search ignores case 
 vim.o.smartcase = true -- unless search term has an uppercase in it
@@ -9,7 +72,7 @@ vim.o.smartcase = true -- unless search term has an uppercase in it
 -- Colors
 vim.o.termguicolors = true
 vim.o.background = 'dark'
---vim.cmd.colorscheme = 'gruvbox'
+vim.cmd.colorscheme('gruvbox')
 
 -- Make splits happen more naturally
 vim.o.splitbelow = true
@@ -66,4 +129,19 @@ vim.api.nvim_create_autocmd("FileType", {
     end
 })
 
--- Options for plugins
+-- Detect janet files as a filetype
+vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
+    pattern = { "*.janet" },
+    callback = function()
+        vim.opt_local.filetype = "janet"
+    end
+})
+
+-- Detect tsv files as a filetype
+vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
+    pattern = { "*.tsv" },
+    callback = function()
+        vim.opt_local.filetype = "tsv"
+    end
+})
+
